35eff0d4a79ca6fbd162e036014f7f4039afd0ca
Tyler Hall <thall2980@gmail.com>
2022-09-13 17:58:12 -0400

    functionality of JS completed

--- index.js
+++ index.js
@@ -1,47 +1,57 @@
 const beerParent = document.querySelector("#beer-list-parent");
-let beerData = [];
+const smallButton = document.querySelector("#small");
+const mediumButton = document.querySelector("#medium");
+const largeButton = document.querySelector("#large");
+const showAllButton = document.querySelector("#show-all");
 
 fetch("https://api.punkapi.com/v2/beers")
   .then((res) => res.json())
   .then((data) => {
-    beerData = data;
     handleFilter(data);
-    beerData.forEach(renderBeers);
-    beerData.forEach(sortBeer);
+    data.forEach(renderBeers);
   });
 
 function handleFilter(beers) {
-  let value = 1000;
-  const smallButton = document.querySelector("#small");
-  const mediumButton = document.querySelector("#medium");
-  const largeButton = document.querySelector("#large");
+  let value;
   smallButton.addEventListener("click", () => {
     value = 5;
-    console.log(value);
     const filterArray = beers.filter((beer) => {
       return beer.abv < value;
     });
-    getData(filterArray);
+    showFilterData(filterArray);
   });
   mediumButton.addEventListener("click", () => {
     value = 10;
-    console.log(value);
     const filterArray = beers.filter((beer) => {
       return beer.abv < value && beer.abv >= 5;
     });
-    getData(filterArray);
+    showFilterData(filterArray);
   });
   largeButton.addEventListener("click", () => {
-    console.log(value);
     const filterArray = beers.filter((beer) => {
       return beer.abv > 10;
     });
-    getData(filterArray);
+    showFilterData(filterArray);
+  });
+  showAllButton.addEventListener("click", () => {
+    value = 0;
+    const filterArray = beers.filter((beer) => {
+      return beer.abv > value;
+    });
+    showFilterData(filterArray);
   });
 }
 
-function getData(filterData) {
-  console.log(filterData);
+function showFilterData(data) {
+  const newBeerList = document.createElement("ol");
+  data.forEach(updateBeers);
+  function updateBeers(data) {
+    beerContainer = document.createElement("div");
+    newBeerList.append(beerContainer);
+    beerContainerCreation(data);
+    beerParent.removeChild(beerParent.firstElementChild);
+    beerParent.appendChild(newBeerList);
+  }
 }
 
 function renderBeers(data) {
@@ -49,6 +59,10 @@ function renderBeers(data) {
   beerContainer = document.createElement("div");
   beerList.append(beerContainer);
 
+  beerContainerCreation(data);
+}
+
+function beerContainerCreation(data) {
   beerName = document.createElement("h1");
   beerName.className = "";
   beerContainer.append(beerName);
@@ -72,7 +86,7 @@ function renderBeers(data) {
   likeImage = document.createElement("img");
   likeImage.src =
     "http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/thumbs-up.png";
-  likeButton = document.createElement("btn");
+  likeButton = document.createElement("button");
   likeButton.textContent = "LIKE";
   let likeCounter = document.createElement("h3");
   likeCounter.textContent = "0";
@@ -87,7 +101,7 @@ function renderBeers(data) {
   dislikeImage = document.createElement("img");
   dislikeImage.src =
     "http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/thumbs-down.png";
-  dislikeButton = document.createElement("btn");
+  dislikeButton = document.createElement("button");
   dislikeButton.textContent = "DISLIKE";
   let dislikeCounter = document.createElement("h3");
   dislikeCounter.textContent = "0";
@@ -96,61 +110,4 @@ function renderBeers(data) {
   dislikeButton.addEventListener("click", () => {
     dislikeCounter.textContent = parseInt(dislikeCounter.textContent) + 1;
   });
-}
-
-function sortBeer(data) {
-  if (data.abv < 4.9) {
-    const newBeerList = document.querySelector("#new-beer-list");
-    beerContainer = document.createElement("div");
-    newBeerList.append(beerContainer);
-    beerName = document.createElement("h1");
-    beerContainer.append(beerName);
-    beerName.textContent = data.name;
-    beerImage = document.createElement("img");
-    beerContainer.append(beerImage);
-    beerImage.src = data.image_url;
-    beerABV = document.createElement("h2");
-    beerContainer.append(beerABV);
-    beerABV.textContent = data.abv;
-    beerDescription = document.createElement("h3");
-    beerContainer.append(beerDescription);
-    beerDescription.textContent = data.description;
-
-    likeContainer = document.createElement("div");
-    beerContainer.append(likeContainer);
-    likeImage = document.createElement("img");
-    likeImage.src =
-      "http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/thumbs-up.png";
-    likeButton = document.createElement("btn");
-    likeButton.textContent = "LIKE";
-    let likeCounter = document.createElement("h3");
-    likeCounter.textContent = "0";
-    likeContainer.append(likeImage, likeButton, likeCounter);
-
-    likeButton.addEventListener("click", () => {
-      likeCounter.textContent = parseInt(likeCounter.textContent) + 1;
-    });
-
-    dislikeContainer = document.createElement("div");
-    beerContainer.append(dislikeContainer);
-    dislikeImage = document.createElement("img");
-    dislikeImage.src =
-      "http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/thumbs-down.png";
-    dislikeButton = document.createElement("btn");
-    dislikeButton.textContent = "DISLIKE";
-    let dislikeCounter = document.createElement("h3");
-    dislikeCounter.textContent = "0";
-    dislikeContainer.append(dislikeImage, dislikeButton, dislikeCounter);
-
-    dislikeButton.addEventListener("click", () => {
-      dislikeCounter.textContent = parseInt(dislikeCounter.textContent) + 1;
-    });
-
-    const lowestABV = document.querySelector("#lowest");
-    lowestABV.addEventListener("click", () => {
-      const beerParent = document.querySelector("#beer-list-parent");
-      beerParent.removeChild(beerParent.firstElementChild);
-      beerParent.appendChild(newBeerList);
-    });
-  }
 }
